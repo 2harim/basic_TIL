10a34f5637c77aa6a37bf65f502c2121eaf39a04
2harim <lhr213213@gmail.com>
2023-04-26 10:53:29 +0900

    [JPA-basic] proxy

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -6,6 +6,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import org.h2.command.ddl.CreateFunctionAlias;
 
 public class JpaMain {
 
@@ -20,11 +21,21 @@ public class JpaMain {
         try {
 
             Member member = new Member();
-            member.setUsername("a");
-            member.setCreatedBy("kim");
-            member.setCreatedDate(LocalDateTime.now());
+            member.setUsername("hello");
 
             em.persist(member);
+
+            em.flush();
+            em.clear();
+
+            Member reference = em.getReference(Member.class, member.getId());
+//            Member findMember = em.find(Member.class, member.getId());
+            Member findMember = em.getReference(Member.class, member.getId());
+            System.out.println("findMember.class = " + findMember.getClass());
+
+            System.out.println("findMember.name = " + findMember.getUsername());
+            System.out.println("findMember.id = " + findMember.getId());
+
 
             tx.commit();
 
