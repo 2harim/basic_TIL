9e6dbe12335bf587ac25526fa0cecc4852b2b63c
2harim <lhr213213@gmail.com>
2023-05-08 16:17:06 +0900

    [Spring Data JPA] Query Method

--- data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -1,8 +1,18 @@
 package study.datajpa.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import study.datajpa.entity.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
+    List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
+
+//    @Query(name = "Member.findByUsername") //주석처리해도 실행 됨! NamedQuery를 먼저 찾음
+    List<Member> findByUsername(@Param("username") String username);
+
+    @Query("select m from Member m where m.username = :username and m.age = :age")
+    List<Member> findUser(@Param("username") String username, @Param("age") int age);
 }
